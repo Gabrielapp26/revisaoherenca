4b59059d4b4dee3b7e744519cca529496bd85b2f
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-02-22 17:50:42 +0000

    Inclusão na classe Empregado

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado extends Pessoa {
+    
+    private double salario;
+    private String cargo;
+
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+}
--- Pessoa.java
+++ Pessoa.java
@@ -8,7 +8,7 @@ public class Pessoa{
         return this.nome;
     }
     public void setNome(String nome) {
-        this.nome=nome;
+        this.nome = nome;
     }
 
     public String getEndereco() {
