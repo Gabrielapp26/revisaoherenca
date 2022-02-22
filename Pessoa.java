b736262b9f4a2dc9596923f0e58a421edefdb1a2
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-02-22 17:34:05 +0000

    Adicionar arquivo

---
+++ Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa{
+    
+    private  String nome;
+    private String endereco;
+    private String telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome=nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
